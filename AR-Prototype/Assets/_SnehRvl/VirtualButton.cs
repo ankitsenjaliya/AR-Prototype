f906f47de581cb979f3b126bbc15816db640ae86
ankitsenjaliya <ankitsenjaliya123@gmail.com>
2018-05-11 20:09:07 -0400

    First commit

---
+++ AR-Prototype/Assets/_SnehRvl/VirtualButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Vuforia; 
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+	public Animation anim1,anim2;
+
+	void Start() {
+		VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+		for (int i = 0; i < vbs.Length; ++i)
+		{
+			vbs[i].RegisterEventHandler(this);
+		}
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb) {
+		if (vb.VirtualButtonName.Equals ("Fire1")) 
+			anim1.Play ("Attack");
+			//p1anim.CrossFadeInFixedTime ("Attack", 0.1f);
+		
+		if (vb.VirtualButtonName.Equals ("Fire2"))
+			anim2.Play ("Attack");
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) { 
+		if (vb.VirtualButtonName.Equals ("Fire1")) 
+			anim1.Play ("idle");
+
+		if (vb.VirtualButtonName.Equals ("Fire2"))
+			anim2.Play ("idle");
+	}
+}
